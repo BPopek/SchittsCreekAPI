82ba80ff20d1d4f4aec3fa369ccc80c028791e2b
BPopek <betsypopek@gmail.com>
2020-04-17 18:30:53 -0600

    testing

--- webpack.config.js
+++ webpack.config.js
@@ -11,22 +11,6 @@ module.exports = {
    devServer: {
       historyApiFallback: true
    },
-   module: {
-      rules: [
-         {
-            test: /\.js$/,
-            use: 'babel-loader',
-         },
-         {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-         },
-         {
-            test: /\.(png|j?g|svg|gif)?$/,
-            use: 'file-loader'
-         }
-]
-   },
    plugins: [
       new HtmlWebPackPlugin({
          template: path.resolve( __dirname, 'public/index.html' ),
